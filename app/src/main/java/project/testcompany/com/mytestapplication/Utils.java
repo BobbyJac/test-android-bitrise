393ed5fdc084a31deb80a829dbf500e34eaa0598
trapacska <trapacska@gmail.com>
2018-02-07 10:35:24 +0100

    First commit

---
+++ app/src/main/java/project/testcompany/com/mytestapplication/Utils.java
@@ -0,0 +1,13 @@
+package project.testcompany.com.mytestapplication;
+
+/**
+ * Created by tamaspapik on 2/6/18.
+ */
+
+public class Utils {
+
+    public static int addNumbers(int first, int second) {
+        return first + second;
+    }
+
+}
---
+++ app/src/test/java/project/testcompany/com/mytestapplication/ExampleUnitTest.java
@@ -0,0 +1,24 @@
+package project.testcompany.com.mytestapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExampleUnitTest {
+
+    @Test
+    public void add_isCorrect() throws Exception {
+        assertEquals(4, Utils.addNumbers(2 , 2));
+    }
+
+    @Test
+    public void add_twodigits_isCorrect() throws Exception {
+        assertEquals(44, Utils.addNumbers(22 , 22));
+    }
+
+    @Test
+    public void add_big_isCorrect() throws Exception {
+        assertEquals(4444+1, Utils.addNumbers(2222 , 2222));
+    }
+
+}
